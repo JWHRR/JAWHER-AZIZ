9501e68a47aa7fad77a21f549de7d066ff2fc87b
azizmahfoudhi <alfredamschel@gmail.com>
2026-04-29 21:47:02 +0200

    Make cards and sidebar translucent to show app background

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -6,7 +6,7 @@ import { AppHeader } from "./AppHeader";
 export function AppLayout({ children }: { children: ReactNode }) {
   return (
     <SidebarProvider>
-      <div className="min-h-screen flex w-full bg-background">
+      <div className="min-h-screen flex w-full bg-transparent">
         <AppSidebar />
         <div className="flex-1 flex flex-col min-w-0">
           <AppHeader />
